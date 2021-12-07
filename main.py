e673ed859eba2ba6095fea29dceabd55a844e600
Rebecamty <66963600+Rebecamty@users.noreply.github.com>
2021-12-07 21:05:12 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,10 @@
+
+#print(WhatError)
+#    print(WhatError)
+#NameError: name 'WhatError' is not defined
+# Print(‘what error is this’)
+#IndentationError: unexpected indent
+#print(what error)
+#SyntaxError: invalid syntax
+ #print(‘what error is this)
+#IndentationError: unexpected indent
